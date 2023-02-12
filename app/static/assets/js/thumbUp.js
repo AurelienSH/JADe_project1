80e62c08a0c2ea012051359980dfb9698f8e1566
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-02-12 20:46:02 +0100

    (feat) Début de l'implémentation de la fonctionnalité pour donner son avis
    J'ai commencé à implémenter la fonctionnalité pour donner son avis :
    
    - J'ai ajouté une classe 'thumb-button' dans main.css : c'est pour
    que les bouttons de cette classe aient une apparence différente des
    autres boutons
    - J'ai ajouté une colonne 'avis' dans le template Jinja : dans la colonne
    apparaît le bouton thumb-up
    - J'ai écrit un script en javascript pour que le bouton change
    de couleur quand on appuie dessus pour que l'utilisateur sache qu'il a déjà cliqué.
    Pour l'instant, ça marche pas. Le bouton ne fait rien quand on clique dessus.

---
+++ app/static/assets/js/thumbUp.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('thumbUpButton');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'salmon';
+  btn.style.color = 'white';
+});
