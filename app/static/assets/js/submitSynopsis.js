bf138f8c776aba33a7c4bac54643baa27dda9d2b
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-02-20 10:17:53 +0100

    (refractor) Bouger le javascript pour l'appel à notre API dans un fichier à part
    Dans le fichier index.html qui correspond à notre interface graphique, le
    javascript dans la balise <script> sert à faire appel à notre API et à écrire le
    résultat de la requête dans la balise <div> avec l'ID "result". J'ai bougé ce
    javascript dans un fichier à part pour bien diviser les tâches et que le code soit
    plus lisible.

---
+++ app/static/assets/js/submitSynopsis.js
@@ -0,0 +1,30 @@
+// Javascript pour récupérer le résultat de l'API et l'écrire au bon endroit
+
+document.querySelector("#inputForm").onsubmit = async (event) => {
+    event.preventDefault() // empêche la page de reload
+    const form = event.target
+    
+    // Récupération de ce qu'on a écrit dans le formulaire
+    const data = {
+        synopsis: form.querySelector("#synopsis").value,
+    }
+    
+    // Définition de la requête POST
+    const response = await fetch(
+        form.action, // l'url de notre API
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "text/html"
+            },
+            body: JSON.stringify(data)
+        }
+        )
+
+    // récupération du résultat de la requête
+    const result = await response.text() // read response body as text
+
+    // écriture du résultat dans la div avec l'ID "result"
+    document.querySelector("#result").innerHTML = result
+}
