6865f75d857c91be54ec03c8c324b6f92fb95fd3
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-02-20 12:08:25 +0100

    (fix) Changement des couleurs des boutons thumb-up/thumb-down
    Avant, le changement des couleurs des boutons lorsqu'un utilisateur
    cliquait n'affectait que la première ligne du tableau à cause
    d'un problème d'ID. J'ai généré des ID unique à l'aide de l'index
    de la boucle dans le template JINJA. Maintenant le changement de couleur
    se fait correctement pour toutes les lignes du tableau :
    
    - Quand l'utilisateur clique sur le thumb-up, il devient vert et le bouton
    thumb-down de la même ligne redevient gris s'il avait déjà été cliqué
    
     - Quand l'utilisateur clique sur le thumb-down, il devient rouge et le bouton
    thumb-up de la même ligne redevient gris s'il avait déjà été cliqué.

--- app/static/assets/js/thumbUp.js
+++ app/static/assets/js/thumbUp.js
@@ -1,15 +1,35 @@
-function thumbUp() {
-  console.log("bouton thumb up cliqué");
-  var thumbDownButton = document.getElementById("thumb-down-button");
-  var thumbUpButton = document.getElementById("thumb-up-button");
-  thumbDownButton.classList.remove("active");
-  thumbUpButton.classList.add("active");
+function thumbUp(button_id) {
+
+  thumb_up_button_clicked = document.getElementById(button_id)
+  const row_number = thumb_up_button_clicked.getAttribute("row_number")
+
+  var row_thumb_down_button = document.getElementById(`thumb-down-button-result-${row_number}`);
+  
+  /*
+  Changement des couleurs : 
+  - thumb-up devient vert
+  - thumb-down redevient gris s'il avait déjà été cliqué avant
+  */
+  row_thumb_down_button.classList.remove("active");
+  thumb_up_button_clicked.classList.add("active");
 }
 
-function thumbDown(){
-  console.log("bouton thumb down cliqué")
-  var thumbDownButton = document.getElementById("thumb-down-button");
-  var thumbUpButton = document.getElementById("thumb-up-button");
-  thumbUpButton.classList.remove("active");
-  thumbDownButton.classList.add("active");
+function thumbDown(button_id){
+  
+  // Sélection du bouton thumb-down cliqué
+  thumb_down_button_clicked = document.getElementById(button_id)
+
+  // Sélection du numéro de la ligne du tableau
+  const row_number = thumb_down_button_clicked.getAttribute("row_number")
+
+  // Sélection du bouton thumb-up sur la même ligne
+  var row_thumb_up_button = document.getElementById(`thumb-up-button-result-${row_number}`);
+  
+  /*
+  Changement des couleurs : 
+  - thumb-down devient rouge
+  - thumb-down redevient gris s'il avait déjà été cliqué avant
+  */
+  row_thumb_up_button.classList.remove("active");
+  thumb_down_button_clicked.classList.add("active");
 }
