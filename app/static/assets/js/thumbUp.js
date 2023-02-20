2302657299fdd55c7bc6de635c6c3ccd43cc555a
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-02-20 12:16:27 +0100

    (docs) Ajout de commentaires

--- app/static/assets/js/thumbUp.js
+++ app/static/assets/js/thumbUp.js
@@ -1,8 +1,12 @@
 function thumbUp(button_id) {
 
+  // Sélection du bouton thumb-up cliqué
   thumb_up_button_clicked = document.getElementById(button_id)
+
+  // Sélection du numéro de la ligne du tableau
   const row_number = thumb_up_button_clicked.getAttribute("row_number")
 
+  // Sélection du bouton thumb-down sur la même ligne
   var row_thumb_down_button = document.getElementById(`thumb-down-button-result-${row_number}`);
   
   /*
