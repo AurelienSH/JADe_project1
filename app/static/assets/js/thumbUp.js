1578ac40a154c379be0904f20fcd4973717e4ade
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-02-22 17:26:01 +0100

    (refractor) Amélioration du script thumbUp.js pour l'action des boutons
    J'ai amélioré le script thumbUp.js. Il fait exactement la même chose mais il y a
    beaucoup moins de répétitions. Avant il y avait une fonction pour l'action du
    thumb-up, et une fonction pour l'action du thumb-down. Maintenant, il n'y en a
    qu'une seule qui s'occupe de supprimer ou d'ajouter la requête de l'utilisateur à
    la database et une autre qui s'occupe du changement des couleurs.

--- app/static/assets/js/thumbUp.js
+++ app/static/assets/js/thumbUp.js
@@ -1,85 +1,70 @@
-function thumbUp(button_id) {
+function clickThumbButtons(button_id, isThumbUp) {
 
-  // Sélection du bouton thumb-up cliqué
-  thumb_up_button_clicked = document.getElementById(button_id)
+  // Sélection du bouton thumb-up/down cliqué
+  const button_clicked = document.getElementById(button_id)
 
-  // Sélection du numéro de la ligne du tableau
-  const row_number = thumb_up_button_clicked.getAttribute("row_number")
+  // Sélection du numéro de la ligne du tableau pour pouvoir récupérer
+  // les autres éléments de la même ligne
+  const row_number = button_clicked.getAttribute("row_number")
 
-  // Sélection du bouton thumb-down sur la même ligne
-  var row_thumb_down_button = document.getElementById(`thumb-down-button-result-${row_number}`);
-  
-  /*
-  Changement des couleurs : 
-  - thumb-up devient vert
-  - thumb-down redevient gris s'il avait déjà été cliqué avant
-  */
-  row_thumb_down_button.classList.remove("active");
-  thumb_up_button_clicked.classList.add("active");
+  // Changement des couleurs des pouces
+  updateThumbColor(isThumbUp, row_number);
 
-  // Récupération des valeurs
+  // Récupération des valeurs pour la suppression ou l'ajout du synopsis à la BDD
 
-  var data =  {
+  const data =  {
     "title": document.getElementById(`title-result-${row_number}`).innerText,
     "date_published": document.getElementById(`date-published-result-${row_number}`).innerText,
     "type": document.getElementById(`type-result-${row_number}`).innerText,
     "content": document.getElementById("input_user").innerText
   }
 
-  // Requête POST pour ajouter la requête comme synopsis de l'oeuvre
-  // à laquelle il a mis un thumb-up
+  let method;
+  // Requête POST pour ajouter si on clique sur thumb-up
+  if (isThumbUp) {
+      method = "POST";
+  }
+  // Requête DELETE pour supprimer de la BDD si on clique sur thumb-down
+  else {
+      method = "DELETE";
+  }
+  
   fetch(
     "http://127.0.0.1:8000/synopsis", // l'url de notre API
     {
-        method: "POST",
+        method: method,
         headers: {
             "Content-Type": "application/json",
             "Accept": "text/html"
         },
         body: JSON.stringify(data)
     }
-    )
-
-
+    );  
 }
 
-function thumbDown(button_id){
+function updateThumbColor(isThumbUp, row_number) {
   
-  // Sélection du bouton thumb-down cliqué
-  thumb_down_button_clicked = document.getElementById(button_id)
+  // Sélection des deux boutons thumb de la ligne row_number
+  const row_thumb_up_button = document.getElementById(`thumb-up-button-result-${row_number}`);
+  const row_thumb_down_button = document.getElementById(`thumb-down-button-result-${row_number}`);
 
-  // Sélection du numéro de la ligne du tableau
-  const row_number = thumb_down_button_clicked.getAttribute("row_number")
+  //Changement des couleurs
 
-  // Sélection du bouton thumb-up sur la même ligne
-  var row_thumb_up_button = document.getElementById(`thumb-up-button-result-${row_number}`);
-  
   /*
-  Changement des couleurs : 
-  - thumb-down devient rouge
-  - thumb-down redevient gris s'il avait déjà été cliqué avant
+  * Si on clique sur thumb-up, il devient vert
+  * Et le thumb-down de la même ligne devient gris s'il avait été cliqué avant.
   */
-  row_thumb_up_button.classList.remove("active");
-  thumb_down_button_clicked.classList.add("active");
-
-  // Récupération des valeurs
-  var data =  {
-    "title": document.getElementById(`title-result-${row_number}`).innerText,
-    "date_published": document.getElementById(`date-published-result-${row_number}`).innerText,
-    "type": document.getElementById(`type-result-${row_number}`).innerText,
-    "content": document.getElementById("input_user").innerText
+  if (isThumbUp) {
+      row_thumb_down_button.classList.remove("active");
+      row_thumb_up_button.classList.add("active");
   }
 
-  // Requête DELETE pour supprimer le synopsis écrit par l'utilisateur de la BDD
-  // pour l'oeuvre sélectionné, si jamais il avait mis un thumb-up avant
-  fetch(
-    "http://127.0.0.1:8000/synopsis", // l'url de notre API
-    {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    }
-    )
+  /*
+  * Si on clique sur le thumb-down, il devient rouge
+  * Et le thumb-up de la ligne devient gris s'il avait été cliqué avant
+  */
+ else {
+      row_thumb_up_button.classList.remove("active");
+      row_thumb_down_button.classList.add("active");
+ }
 }
