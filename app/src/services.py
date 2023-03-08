494b16a0e83048fa62b7ddd970ba45828dba3211
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-03-08 13:52:36 +0100

    (chore) Création des fonctions pour la V2 de l'API
    Tâches de la mise en place de la V2 de l'API :
    
    - Création des fonctions utiles pour la V2 de l'API :
    
        - Fonction pour vérifier si un film a déjà eu une review, création
        du film dans la BDD sinon
    
        - Ajout d'une review à ce film : on ajoute la query écrite par l'utilisateur
        dans la liste "pos_query" du film si l'utilisateur a mis un thumb-up pour ce
        film, si l'utilisateur a mis un thumb-down, on l'ajoute à la liste "neg_query"
    
    - Déplacement du chargement du corpus et des modèles dans main car ça faisait plus
    sens : ça permet de pouvoir utiliser la même fonction "cosine_similarity()" avec
    différents modèles

--- app/src/services.py
+++ app/src/services.py
@@ -30,71 +30,93 @@ def get_db() -> _orm.Session: # type: ignore
         yield db
     finally:
         db.close()
-        
-################# CHARGEMENT DU MODELE et des EMBEDDINGS ##################
-
-models_path = "../models"
-embeddings_path = "../embeddings"
-
-# Récupération du modèle depuis le fichier pickled
-with open(f"{models_path}/sentence_similarity_model", "rb") as file:
-    sentence_similarity_model = pickle.load(file)
-
-# Récupération de la liste d'objets Oeuvre depuis le fichier pickled
-with open(f"{embeddings_path}/embeddings_corpus_movie", "rb") as file:
-    embeddings_corpus_movie = pickle.load(file)
 
 ################################# CRUD ###################################
 
-def create_synopsis(db: _orm.Session, synopsis: _schemas.SynopsisCreate) -> _schemas.DBSynopsis:
-    db_synopsis = _models.Synopsis(**synopsis.dict())
-    db.add(db_synopsis)
-    db.commit()
-    db.refresh(db_synopsis)
-    return db_synopsis
+# def create_synopsis(db: _orm.Session, synopsis: _schemas.SynopsisCreate) -> _schemas.DBSynopsis:
+#     db_synopsis = _models.Synopsis(**synopsis.dict())
+#     db.add(db_synopsis)
+#     db.commit()
+#     db.refresh(db_synopsis)
+#     return db_synopsis
 
-def create_query(
-    db: _orm.Session, 
-    query: _schemas.QueryCreate
-) -> _schemas.Query:
-    db_query = _models.Query(**query.dict())
-    db.add(db_query)
-    db.commit()
-    db.refresh(db_query)
-    return db_query
+# def create_query(
+#     db: _orm.Session, 
+#     query: _schemas.QueryCreate
+# ) -> _schemas.Query:
+#     db_query = _models.Query(**query.dict())
+#     db.add(db_query)
+#     db.commit()
+#     db.refresh(db_query)
+#     return db_query
 
-def find_synopsis_containing_word(
-    db: _orm.Session,
-    input: str):
+# def find_synopsis_containing_word(
+#     db: _orm.Session,
+#     input: str):
     
-    return db.query(_models.Synopsis.title, _models.Synopsis.type, _models.Synopsis.date_published, _models.Synopsis.content).where(_models.Synopsis.content.contains(input)).limit(5).all()
+#     return db.query(_models.Synopsis.title, _models.Synopsis.type, _models.Synopsis.date_published, _models.Synopsis.content).where(_models.Synopsis.content.contains(input)).limit(5).all()
 
-def delete_synopsis(
-    synopsis_to_delete: _schemas.SynopsisCreate,
+# def delete_synopsis(
+#     synopsis_to_delete: _schemas.SynopsisCreate,
+#     db: _orm.Session,
+# ):
+    
+#     # Sélection du synopsis à supprimer de la BDD
+#     db_synopsis_to_delete = db.query(_models.Synopsis).where(
+#         and_(_models.Synopsis.title == synopsis_to_delete.title,
+#         _models.Synopsis.date_published == synopsis_to_delete.date_published,
+#         _models.Synopsis.type == synopsis_to_delete.type, 
+#         _models.Synopsis.content == synopsis_to_delete.content)).first()
+    
+#     # Vérification qu'il se trouve bien dans la BDD
+#     if db_synopsis_to_delete:
+#         db.delete(db_synopsis_to_delete)
+#         db.commit()
+#     else:
+#         raise _fastapi.HTTPException(status_code=404, detail="Synopsis not found")
+    
+#     return {
+#         "message": f"Suppresion du synopsis effectuée."
+#     }
+    
+def create_new_review(
     db: _orm.Session,
+    new_review: _schemas.ReviewCreate
+):
+    existing_review = db.query(_models.Review).filter(_models.Review.title.ilike(new_review.title)).first()
+    
+    if existing_review: # Film a déjà eu des reviews
+        return existing_review
+    
+    else: # Première review pour ce film donc ajout dans la BDD
+        db_review = _models.Review(**new_review.dict())
+        db.add(db_review)
+        db.commit()
+        db.refresh(db_review)
+    return db_review
+    
+def add_movie_review(
+    db: _orm.Session,
+    review: _schemas.ReviewCreate,
+    value: str
 ):
     
-    # Sélection du synopsis à supprimer de la BDD
-    db_synopsis_to_delete = db.query(_models.Synopsis).where(
-        and_(_models.Synopsis.title == synopsis_to_delete.title,
-        _models.Synopsis.date_published == synopsis_to_delete.date_published,
-        _models.Synopsis.type == synopsis_to_delete.type, 
-        _models.Synopsis.content == synopsis_to_delete.content)).first()
+    # Vérification de la présence du film dans la BDD
+    # Sinon ajout dans la BDD 
+    db_review = create_new_review(db=db, new_review=review)
     
-    # Vérification qu'il se trouve bien dans la BDD
-    if db_synopsis_to_delete:
-        db.delete(db_synopsis_to_delete)
-        db.commit()
-    else:
-        raise _fastapi.HTTPException(status_code=404, detail="Synopsis not found")
+    if value == "pos": # Review positive
+        db_review.pos_query.append(review.query)
     
-    return {
-        "message": f"Suppresion du synopsis effectuée."
-    }
+    elif value == "neg": # Review négative
+        db_review.neg_query.append(review.query)
+        
+    return db_review
+        
     
 ####################################################################################
 
-def cosine_similarity(user_input: str, oeuvres: List[Tuple] =  embeddings_corpus_movie, model = sentence_similarity_model, k: int = 5):
+def cosine_similarity(user_input: str, oeuvres: List[Tuple], model, k: int = 5):
     
     return _sentence_similarity.get_similar_works(user_input=user_input, oeuvres=oeuvres, model=model) # type: ignore
     
