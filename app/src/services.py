ac334442d93e99257657a643878368690d800365
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-03-13 14:20:48 +0100

    (docs) Ajout de docstrings et commentaires

--- app/src/services.py
+++ app/src/services.py
@@ -51,9 +51,24 @@ def get_db() -> _orm.Session: # type: ignore
 def add_movie_review(
     db: _orm.Session,
     review: _schemas.ReviewAdd
-):
+) -> _schemas.DBReview:
+    """Ajouter une review à la BDD.
+    
+    Cette fonction permet, à partir d'un objet `ReviewAdd`, d'ajouter une
+    review à la table `movie_review` de la BDD.
 
+    Args:
+        db (_orm.Session): Une session permettant d'accéder à la BDD
+        review (_schemas.ReviewAdd): la review à ajouter à la BDD
+
+    Returns:
+        _schemas.DBReview: la review effectivement ajoutée à la BDD
+    """
+
+    # Création de l'objet Review correspondant à une entrée de la table `movie_review`
     db_review = _models.Review(**review.dict())
+    
+    # Ajout de l'entrée à la table
     db.add(db_review)
     db.commit()
     db.refresh(db_review)
@@ -61,6 +76,22 @@ def add_movie_review(
     return db_review
 
 def get_data_for_FT(db: _orm.Session) -> List[InputExample]:
+    """Permet d'obtenir les données nécessaires pour le finetuning du modèle.
+    
+    Cette fonction renvoie toutes les données d'une `db` sous la forme nécessaire
+    pour le finetuning de notre modèle. C'est-à-dire sous une liste de 
+    `InputExample(texts=[synopsis, query], label)`. 
+    
+    Si la review est négative, on attribue le label 1.5 (distance très grande => synopsis
+    et query ne sont pas similaires), si la review est positive 0.2 (distance très petite => 
+    synopsis et query sont similaires).
+
+    Args:
+        db (_orm.Session): la session permettant d'accéder à la BDD
+
+    Returns:
+        List[InputExample]: les données d'entraînement dans le bon format
+    """
     
     train_examples = []
     
